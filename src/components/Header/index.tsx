783bbc09df2f0bebac9023f505fcd0ac9ef5de70
Kai-Mikael Alanne <kaimikael.alanne@icloud.com>
2019-10-24 17:07:44 +0300

    Header styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import {
   faFacebookSquare,
   faInstagram,
 } from '@fortawesome/free-brands-svg-icons';
-import { faPhoneAlt, faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './index.css';
 import { Container } from 'react-bootstrap';
 
@@ -17,20 +17,23 @@ export const Header: React.FC = () => {
       <Container>
         <div className="header-left">
           <a href={`tel:${phoneNumber}`}>
-            <FontAwesomeIcon icon={faPhoneAlt}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faPhoneAlt} size="lg"></FontAwesomeIcon>
             {phoneNumberDisplay}
           </a>
           <a href="mailto:&#105;&#110;&#102;&#111;&#064;&#112;&#105;&#108;&#118;&#105;&#108;&#105;&#110;&#110;&#097;&#110;&#108;&#101;&#105;&#112;&#111;&#109;&#111;&#046;&#102;&#105;">
-            <FontAwesomeIcon icon={faEnvelopeOpen}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faEnvelope} size="lg"></FontAwesomeIcon>
             &#105;&#110;&#102;&#111;&#064;&#112;&#105;&#108;&#118;&#105;&#108;&#105;&#110;&#110;&#097;&#110;&#108;&#101;&#105;&#112;&#111;&#109;&#111;&#046;&#102;&#105;
           </a>
         </div>
         <div className="header-right">
           <a href="https://www.facebook.com/Pilvilinnan-leipomo-1541890192566923/">
-            <FontAwesomeIcon icon={faFacebookSquare}></FontAwesomeIcon>
+            <FontAwesomeIcon
+              icon={faFacebookSquare}
+              size="lg"
+            ></FontAwesomeIcon>
           </a>
           <a href="https://www.instagram.com/pilvilinnan_anna/">
-            <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faInstagram} size="lg"></FontAwesomeIcon>
           </a>
         </div>
       </Container>
